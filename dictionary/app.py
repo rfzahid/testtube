978c5713f7bb0091655de16af8c06cbb24c8862c
Zahid Wakeel <zhdwkl@gmail.com>
2018-03-01 19:59:48 +0530

    Dictionary program done

---
+++ dictionary/app.py
@@ -0,0 +1,34 @@
+import json
+import difflib
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+
+word = input("Enter a word: ").lower()
+
+def check(word):
+    if word in data:
+        meaning = data[word]
+        return meaning
+    else:
+        match = get_close_matches(word, data.keys(), cutoff = 0.8)
+        if(match != []):
+            flag = input("Did you mean " + match[0] + "? " + "If yes, press Y otherwise press N: ")
+            if flag == "Y":
+                meaning = data[match[0]]
+                return meaning
+            elif flag == "N":
+                meaning = "No such word exists!"
+                return meaning
+            else:
+                meaning = "Wrong key pressed -_-"
+                return meaning
+        else:
+            meaning = "No such word exists!"
+            return meaning
+
+ans = check(word)
+if type(ans) == list:
+    for i in ans:
+        print(i)
+else:
+    print(ans)
