cff1cb7c74b4b80ac0840601cdb7545d77d57e49
Zahid Wakeel <zhdwkl@gmail.com>
2018-03-07 00:54:06 +0530

    Working code to grab ATP rankings data

---
+++ web scraping/scrape.py
@@ -0,0 +1,53 @@
+
+import requests
+import pandas
+import collections
+from bs4 import BeautifulSoup
+import datetime
+import arsenal
+
+rankDate = input("Provide date: ")
+rankRange = input("Provide range: ")
+
+url = arsenal.generateURL(rankDate, rankRange)
+r = requests.get(url)
+c = r.content
+soup = BeautifulSoup(c, "html.parser")
+
+# Grabbing all the dates
+
+date_list = []
+all = soup.find("ul", {"data-value":"rankDate"}).find_all("li")
+for each in all:
+    date_list.append(each.text.replace("\t","").replace("\n","").replace("\r","").replace(".","-"))
+# print(date_list)
+
+if arsenal.isDateValid(rankDate, date_list) == False:
+    print("No ATP rankings released on this date!")
+else:
+    # Grabbing all the column names
+    record = {}
+    all = soup.find("table", {"class":"mega-table"})
+    thead = all.find_all("div", {"class":"sorting-label"})
+    for each in thead:
+        record[each.text.replace("\t","").replace("\r","").replace("\n","")] = ""
+    # print(record)
+
+    # Grabbing all the player data as requested
+    classes = ["rank-cell", "move-cell", "country-cell", "player-cell", "age-cell", "points-cell", "tourn-cell", "pts-cell", "next-cell"]
+    tbody = all.find("tbody").find_all("tr")
+    for r,c in zip(record, classes):
+        print("Grabbing " + c + "...")
+        temp = []
+        for row in tbody:
+            try:
+                temp.append(row.find("td", {"class":c}).text.replace("\n","").replace("\r","").replace("\t",""))
+            except:
+                pass
+        record[r] = temp
+    record = collections.OrderedDict(record)
+    # print(record)
+
+    df = pandas.DataFrame(record)
+    df.to_csv("ATP rankings.csv")
+    print("Done :) Output data exported to ATP rankings.csv file")
